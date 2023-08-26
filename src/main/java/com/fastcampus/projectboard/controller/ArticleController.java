e075c5570a7bc3162b1a665828367780bf79ce62
uknow <yunhoknight@naver.com>
2023-08-26 22:33:52 +0900

    Feat:게시판 페이지 구현
    - ArticleController 구현
    - 게시글 페이지 작성

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -4,6 +4,7 @@ package com.fastcampus.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,4 +26,14 @@ public class ArticleController {
 
         return "articles/index";
     }
+
+        @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+            map.addAttribute("article", "null"); //TODO: 추후에 DTO로 구현
+            map.addAttribute("articleComment", List.of());
+
+        return "articles/detail";
+    }
+
+
 }
