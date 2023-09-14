9ed65b9c044a967ce6fb1f89f643d50ef205cab1
uknow <yunhoknight@naver.com>
2023-09-14 16:08:00 +0900

    Feat: 대댓글 기능 구현 - 기능 업데이트

--- src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -14,36 +14,33 @@ public record ArticleCommentDto(
         Long id,
         Long articleId,
         UserAccountDto userAccountDto,
+        Long parentCommentId,
         String content,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static ArticleCommentDto of(Long id,
-                                       Long articleId,
-                                       UserAccountDto userAccountDto,
-                                       String content,
-                                       LocalDateTime createdAt,
-                                       String createdBy,
-                                       LocalDateTime modifiedAt,
-                                       String modifiedBy) {
-        return new ArticleCommentDto(id,
-                articleId,
-                userAccountDto,
-                content,
-                createdAt,
-                createdBy,
-                modifiedAt,
-                modifiedBy);
-    }
 
     public static ArticleCommentDto of(Long articleId,
                                        UserAccountDto userAccountDto,
                                        String content) {
-        return new ArticleCommentDto(null,
+        return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                null,
+                content);
+    }
+
+    public static ArticleCommentDto of(Long articleId,
+                                       UserAccountDto userAccountDto,
+                                       Long parentCommentId,
+                                       String content) {
+        return ArticleCommentDto.of(
+                null,
+                articleId,
+                userAccountDto,
+                parentCommentId,
                 content,
                 null,
                 null,
@@ -51,11 +48,34 @@ public record ArticleCommentDto(
                 null);
     }
 
+    public static ArticleCommentDto of(Long id,
+                                       Long articleId,
+                                       UserAccountDto userAccountDto,
+                                       Long parentCommentId,
+                                       String content,
+                                       LocalDateTime createdAt,
+                                       String createdBy,
+                                       LocalDateTime modifiedAt,
+                                       String modifiedBy) {
+        return new ArticleCommentDto(
+                id,
+                articleId,
+                userAccountDto,
+                parentCommentId,
+                content,
+                createdAt,
+                createdBy,
+                modifiedAt,
+                modifiedBy);
+    }
+
+
     public static ArticleCommentDto from(ArticleComment entity) {
         return new ArticleCommentDto(
                 entity.getId(),
                 entity.getArticle().getId(),
                 UserAccountDto.from(entity.getUserAccount()),
+                entity.getParentCommentId(),
                 entity.getContent(),
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),
