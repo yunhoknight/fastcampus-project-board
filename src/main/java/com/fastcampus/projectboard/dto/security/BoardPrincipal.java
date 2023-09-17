e0b39ed65bc488f0bd8b6ecd90a3298936262016
uknow <yunhoknight@naver.com>
2023-09-17 22:08:25 +0900

    Feat: 카카오톡 로그인 기능 구현 #64

--- src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
-    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+    public static BoardPrincipal of(String username,
+                                    String password,
+                                    String email,
+                                    String nickname,
+                                    String memo) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -31,7 +38,29 @@ public record BoardPrincipal(
                         .collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo);
+                memo,
+                Map.of());
+    }
+
+    public static BoardPrincipal of(String username,
+                                    String password,
+                                    String email,
+                                    String nickname,
+                                    String memo,
+                                    Map<String, Object> oAuth2Attributes) {
+        Set<RoleType> roleTypes = Set.of(RoleType.USER);
+
+        return new BoardPrincipal(
+                username,
+                password,
+                roleTypes.stream()
+                        .map(RoleType::getName)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toUnmodifiableSet()),
+                email,
+                nickname,
+                memo,
+                oAuth2Attributes);
     }
 
     public static BoardPrincipal from(UserAccountDto dto) {
@@ -88,6 +117,12 @@ public record BoardPrincipal(
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public Map<String, Object> getAttributes() { return oAuth2Attributes; }
+
+    @Override
+    public String getName() { return username; }
 
     public enum RoleType {
         USER("ROLE_USER");
